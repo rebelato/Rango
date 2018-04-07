1859e23558733342031362a610231254ec669fbb
Eu <lucas@lucas-2.local>
2018-04-07 18:16:13 -0300

    Table view

--- Rango/ViewController/HomeViewController.swift
+++ Rango/ViewController/HomeViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-class HomeViewController: UIViewController{
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var companyTableView: UITableView!
+    
+    let companyName = ["Restaurante 1", "Restaurante 2", "Restaurante 3"]
+    let share = ["HDUOHAOUDHWOUHDOAUWHDhuheuheuwhfuewhfuwehfuwehfuewhfuewhfuwehfuwehfuwehfuwehfuwhfuwhfuwehfuwhfuwehfuwehfuwhfuhweufhweufheuwhfuwehfuweOUAH", "jannawnawonawonxaown", "ijijciqjiqwdoqnwdoq"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        companyTableView.delegate = self
+        companyTableView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return companyName.count
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = companyTableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = companyName[indexPath.row]
+        cell?.detailTextLabel?.text = "\(share[indexPath.row])"
+        cell?.imageView?.image = UIImage(named: "Banana")
+        
+        return cell!
     }
     
     
